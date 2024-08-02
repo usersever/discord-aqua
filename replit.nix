37d867b1bfaea2c01df4db2c9d6beefcf6b18137
Bảo Nguyễn <113667748+usersever@users.noreply.github.com>
2024-08-02 08:39:33 +0000

    .nix

--- replit.nix
+++ replit.nix
@@ -1,10 +1,16 @@
 {pkgs}: {
-  deps = [
-    pkgs.nlohmann_json
-    pkgs.libsodium
-    pkgs.libopus
-    pkgs.openssl
-    pkgs.zlib
-    pkgs.cmake
-  ];
-}
+   deps = [
+     pkgs.nlohmann_json
+     pkgs.libsodium
+     pkgs.libopus
+     pkgs.openssl
+     pkgs.zlib
+     pkgs.cmake
+     pkgs.pkg-config
+     pkgs.libldap
+     pkgs.libpsl
+     pkgs.libssh2
+     pkgs.libpthreadstubs
+     pkgs.libmysqlclient
+   ];
+ }
